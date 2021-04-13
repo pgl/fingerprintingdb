672d727ce5c480f0cb682317101e30d843ac6b89
Peter Lowe <pgl@yoyo.org>
2021-04-14 01:07:01 +0200

    Massively reduced scripts to check for from FP-Inspector

--- checks/fpinspector-scripts.check.sh
+++ checks/fpinspector-scripts.check.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-## Checks against any of the known scripts from the FP Inspector research
+## Checks against some of the known scripts from the FP Inspector research
 ##
 ##  https://raw.githubusercontent.com/uiowa-irl/FP-Inspector/master/Data/fingerprinting_domains.json
+##
+## Needed cleaning as there were entries like:
+##
+##    "b9505169dd0162609b94dc6d737eecc6": [
+##        {
+##            "script_url": "https://www.flickr.com/",
+##            "top_url": "https://www.flickr.com/"
+##        }
+##    ],
+##
+## As a result, checked for number of occurrences of each script from the
+## above URL, and just took everything with more than 25 or more counts.
+##
 ##
 
 if [ ! "$1" ]
